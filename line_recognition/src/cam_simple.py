5973b42f1ee406ad93621918bcab2c6f3a32a8c7
Alexander <51728129+Alex-T-RU-DE@users.noreply.github.com>
2021-06-11 05:33:58 +0300

    Update cam_simple.py

--- line_recognition/src/cam_simple.py
+++ line_recognition/src/cam_simple.py
@@ -55,45 +55,43 @@ class image_rec(object):
 		image_blur=cv2.GaussianBlur(image_array, (7,7), 1)
 		#yellow
 		if color==1:
-			lower=np.array([15,138,64])			
-			upper=np.array([97,255,192])
+			lower=np.array([15, 138, 64])			
+			upper=np.array([97, 255, 192])
 		#red
 		elif color==2:
-			lower=np.array([0,0,191])			
-			upper=np.array([79,255,255])
+			lower=np.array([0, 0, 191])			
+			upper=np.array([79, 255, 255])
 		#blue
 		elif color==3:
-			lower=np.array([59,0,0])			
-			upper=np.array([136,50,65])
-		mask=cv2.inRange(image_array,lower,upper)
-		result=cv2.bitwise_and(image_array,image_array, mask = mask)
-		imgCanny=cv2.Canny(mask,23,23)
-		kernel=np.ones((5,5))
-		imgDil=cv2.dilate(imgCanny, kernel, iterations=1)
-		self.getContours(imgDil,imgContour)            
+			lower=np.array([59, 0, 0])			
+			upper=np.array([136, 50, 65])
+		mask=cv2.inRange(image_array, lower, upper)
+		result=cv2.bitwise_and(image_array, image_array, mask = mask)
+		imgCanny=cv2.Canny(mask, 23, 23)
+		kernel=np.ones((5, 5))
+		imgDil=cv2.dilate(imgCanny, kernel, iterations = 1)
+		self.getContours(imgDil, imgContour)            
             
 		return imgContour, imgDil
             
                  
             
-	def getContours(self,img, imgContour):
+	def getContours(self, img, imgContour):
 
 		contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2:]
-		msgobj_array=tape_msgs_array()
+		msgobj_array = tape_msgs_array()
 		for cnt in contours:
-			area=cv2.contourArea(cnt)
-			if area>300:
-				msgobj=tape_msgs()
-				cv2.drawContours(imgContour, cnt, -1, (0,255,0), 3)
+			area = cv2.contourArea(cnt)
+			if area > 300:
+				msgobj = tape_msgs()
+				cv2.drawContours(imgContour, cnt, -1, (0, 255, 0), 3)
 				peri=cv2.arcLength(cnt, True)
-				approx=cv2.approxPolyDP(cnt, 0.02*peri, True)
+				approx=cv2.approxPolyDP(cnt, 0.02 * peri, True)
 				x, y, w, h= cv2.boundingRect(approx)
-				#print("coordinates: ")
-				#print((x+w/2),(y+h/2))
-				cv2.rectangle(imgContour, (x, y), (x+w, y+h), (255, 0, 0), 2)
-				msgobj.posX=(x+w/2)
-				msgobj.posY=(y+h/2)
-				msgobj.length=0
+				cv2.rectangle(imgContour, (x, y), (x + w, y + h), (255, 0, 0), 2)
+				msgobj.posX = (x + w / 2)
+				msgobj.posY = (y + h / 2)
+				msgobj.length = 0
 				msgobj_array.tape_msgs_array.append(msgobj)
 		self.pub.publish(msgobj_array)
 
@@ -107,5 +105,5 @@ def main():
 		print("Shutting down the program")
 		
 
-if __name__=='__main__':
+if __name__ == '__main__':
 	main()
